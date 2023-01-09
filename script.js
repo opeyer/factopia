34df62df838e7a0b7ff409ed4f3576c093f06f53
Otto William Peyer IV <opeyer@gmail.com>
2023-01-08 19:41:29 -0600

    JS script work bookmark

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Show/hide share form
+
 const btn = document.querySelector(".btn-open");
 const form = document.querySelector(".fact-form");
 
@@ -10,3 +12,68 @@ btn.addEventListener("click", function () {
     btn.textContent = "Share";
   }
 });
+
+/*
+let votesInteresting = 23;
+let votesMindblowing = 5;
+const text = "Lisbon is the capital of Portugal";
+
+votesInteresting = votesInteresting + 1;
+votesInteresting++;
+console.log(votesInteresting);
+
+let totalUpvotes = votesInteresting + votesMindblowing;
+console.log("Upvotes:", totalUpvotes);
+
+let votesFalse = 4;
+const isCorrect = votesFalse < totalUpvotes;
+console.log(isCorrect);
+*/
+
+// Calculate how old a post is
+
+function calcFactAge(year) {
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - year;
+
+  if (age >= 0) return age;
+  else return "ERROR: Impossible year! Are you a time traveler?";
+
+  return age;
+}
+
+const age1 = calcFactAge(2015);
+console.log(age1);
+console.log(calcFactAge(2020));
+console.log(calcFactAge(1990));
+console.log(calcFactAge(2037));
+
+let votesInteresting = 20;
+let votesMindblowing = 5;
+
+if (votesInteresting === votesMindblowing) {
+  alert("This fact is equally interesting and mindblowing!");
+} else if (votesInteresting > votesMindblowing) {
+  console.log("Interesting fact...");
+} else if (votesInteresting < votesMindblowing) {
+  console.log("Wow! Mindblowing fact!!!");
+}
+
+// falsy values: 0, '', null, undefined
+// truthy value: everything else...
+if (votesMindblowing) {
+  console.log("MINDBLOWING FACT!!!");
+} else {
+  console.log("Not so special...");
+}
+
+let votesFalse = 87;
+const totalUpvotes = votesInteresting + votesMindblowing;
+
+const message =
+  totalUpvotes > votesFalse
+    ? "The fact is true."
+    : "Might be false, check other sources.";
+
+// alert(message);
+// 3 > 7 ? 'A' : 'B'
