d39c940433eab9f61c5b351be13bf8738089a4b8
Otto William Peyer IV <opeyer@gmail.com>
2023-01-12 00:52:25 -0600

    Rough layout composed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 import "./style.css";
 
 function App() {
-  return (
-    <header className="header">
-      <div className="logo">
-        <img src="logo.png" height="68px" width="68px" alt="Factopia logo" />
-        <h1>Factopia</h1>
-      </div>
+  const appTitle = "Factopia";
 
-      <button className="btn btn-large btn-open">Share</button>
-    </header>
+  return (
+    <>
+      {/* HEADER */}
+      <header className="header">
+        <div className="logo">
+          <img src="logo.png" height="68px" width="68px" alt="Factopia logo" />
+          <h1>{appTitle}</h1>
+        </div>
+
+        <button className="btn btn-large btn-open">Share</button>
+      </header>
+
+      <ShareForm />
+
+      <main class="main">
+        <CategoryFilter />
+        <FactList />
+      </main>
+    </>
   );
+}
+
+function ShareForm() {
+  return <form className="fact-form">Share a fact</form>;
+}
+
+function CategoryFilter() {
+  return <aside>Category Filter</aside>;
+}
+
+function FactList() {
+  return <section>Facts list</section>;
 }
 
 export default App;
