9fe5523b3d9113c03626fabf9b69f27b339ee7a9
Otto William Peyer IV <opeyer@gmail.com>
2023-01-13 14:30:07 -0600

    Building the form

--- src/App.js
+++ src/App.js
@@ -83,10 +83,6 @@ function Header({ showForm, setShowForm }) {
   );
 }
 
-function ShareForm() {
-  return <form className="fact-form">Share a fact</form>;
-}
-
 const CATEGORIES = [
   { name: "culture", color: "#d2bed4" },
   { name: "entertainment", color: "#d8aabf" },
@@ -97,6 +93,45 @@ const CATEGORIES = [
   { name: "science", color: "#6ca080" },
   { name: "technology", color: "#94b2e2" },
 ];
+
+function ShareForm() {
+  const [text, setText] = useState("");
+  const [source, setSource] = useState("");
+  const [category, setCategory] = useState("");
+  const textLength = text.length;
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(text, source, category);
+  }
+
+  return (
+    <form className="fact-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Share a fact with the world..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <span>{200 - textLength}</span>
+      <input
+        value={source}
+        type="text"
+        placeholder="Trustworthy source..."
+        onChange={(e) => setSource(e.target.value)}
+      />
+      <select value={category} onChange={(e) => setCategory(e.target.value)}>
+        <option value="">Choose category:</option>
+        {CATEGORIES.map((cat) => (
+          <option key={cat.name} value={cat.name}>
+            {cat.name.toUpperCase()}
+          </option>
+        ))}
+      </select>
+      <button className="btn btn-large">Post</button>
+    </form>
+  );
+}
 
 function CategoryFilter() {
   return (
